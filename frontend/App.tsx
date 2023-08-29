c3ff925f04415a8b2dc8ca9b2fc2b9df89aefacc
CatherineJOnia <catherinejonia@gmail.com>
2023-08-29 16:15:51 -0400

    App.css and App.tsx

---
+++ frontend/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Trips from "./components/TripsPage";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trips" element={<Trips />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
