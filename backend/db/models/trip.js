1621f02c14f6d0e6791ab7f1c149a5cfccae7fc6
CatherineJOnia <catherinejonia@gmail.com>
2023-08-29 14:34:53 -0400

    db models trip

---
+++ backend/db/models/trip.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const Trip = sequelize.define(
+    "Trip",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {}
+  );
+  Trip.associate = function (models) {
+    // associations can be defined here
+    Trip.hasMany(models.Location, {
+      foreignKey: "tripId",
+    });
+    Trip.belongsTo(models.users, {
+      foreignKey: {
+        name: "userId",
+        allowNull: false,
+        onDelete: "CASCADE",
+      },
+    });
+  };
+  return Trip;
+};
