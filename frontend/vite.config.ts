47b43822e9cb7795149851c7ef0c876883c423ed
Trevor Mathiak <tmathiak@gmail.com>
2023-09-18 15:08:55 -0400

    Add vite-tsconfig-paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 // import { defineConfig } from 'vite'
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteTsconfigPaths()],
+});
