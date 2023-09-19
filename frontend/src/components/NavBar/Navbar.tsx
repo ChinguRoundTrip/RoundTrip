c1a634f20a2759c99cf2c26876e5376c8db6f24c
Trevor Mathiak <tmathiak@gmail.com>
2023-09-18 22:25:34 -0400

    Fix path to RoundTrip logo in navbar

--- frontend/src/components/NavBar/Navbar.tsx
+++ frontend/src/components/NavBar/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
       <div className="flex items-center justify-between px-5">
         <Link to="/">
           <img
-            src="../../public/RoundTrip.png"
+            src="/RoundTrip.png"
             alt="logo"
             width={100}
             height={100}
